6ddaaba30c4e6a64ecb1f192fb84364d10ea05a8
ahmedX999 <aaittaleb.354@gmail.com>
2024-06-06 22:16:40 +0100

    first commit

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
-output "lambda_function_name" {
-  value = length(data.aws_lambda_function.existing_lambda) > 0 ? aws_lambda_function.lambda_function[0].function_name : null
+
+output "message" {
+  value = "Hello World"
 }
+
