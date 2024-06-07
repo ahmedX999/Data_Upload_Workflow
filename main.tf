3e56500f084f4bfb60b64faee48c3ce7d5d9220f
ahmedX999 <aaittaleb.354@gmail.com>
2024-06-07 01:14:18 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-// Define a local variable to check if the Lambda function exists
-data "aws_lambda_function" "existing_lambda" {
-  function_name = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda"
+provider "aws" {
+  region = "us-east-1"
 }
 
-locals {
-  lambda_function_exists = length(data.aws_lambda_function.existing_lambda) > 0
+
+variable "lambda_function_exists" {
+  default = length(data.aws_lambda_function.existing_lambda) > 0
 }
 
-// Define IAM role, Lambda function, and policy attachment conditionally based on the existence of the Lambda function
+// Create IAM role, Lambda function, and policy attachment conditionally based on the existence of the Lambda function
 resource "aws_iam_role" "lambda_exec_role" {
-  count = local.lambda_function_exists ? 0 : 1
+  count = var.lambda_function_exists ? 0 : 1
 
   name = "lambda_exec_role"
 
@@ -28,17 +28,22 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 
 resource "aws_lambda_function" "lambda_function" {
-  count = local.lambda_function_exists ? 0 : 1
+  count = var.lambda_function_exists ? 0 : 1
 
   function_name = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda"
-  role          = aws_iam_role.lambda_exec_role[0].arn
+  role          = aws_iam_role.lambda_exec_role[count.index].arn
   package_type  = "Image"
   image_uri     = var.instance_type == "google_drive" ? "aws_account_id.dkr.ecr.us-east-1.amazonaws.com/lambda-docker-google-drive:latest" : "aws_account_id.dkr.ecr.us-east-1.amazonaws.com/lambda-docker-s3-bucket:latest"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
-  count = local.lambda_function_exists ? 0 : 1
+  count = var.lambda_function_exists ? 0 : 1
 
-  role       = aws_iam_role.lambda_exec_role[0].name
+  role       = aws_iam_role.lambda_exec_role[count.index].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Output the Lambda function name if it exists, otherwise output null
+output "lambda_function_name" {
+  value = var.lambda_function_exists ? null : aws_lambda_function.lambda_function[0].function_name
 }
