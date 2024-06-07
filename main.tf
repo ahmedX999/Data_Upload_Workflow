db08bcbab8aea711b40f93f728c8f2b5ec44c7d4
ahmedX999 <aaittaleb.354@gmail.com>
2024-06-07 01:27:57 +0100

    first commit

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "us-east-1" # Specify your region here
 }
 
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
-  type        = string
-  default     = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda"
+
+
+locals {
+  lambda_function_name = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda"
 }
 
 # Data source to check if the Lambda function exists
 data "aws_lambda_function" "existing_lambda" {
-  function_name = var.lambda_function_name
+  function_name = local.lambda_function_name
 
   # Using a count to conditionally fetch the data or not
   count = length(try([aws_lambda_function.existing_lambda.arn], [])) == 1 ? 1 : 0
@@ -20,7 +20,7 @@ data "aws_lambda_function" "existing_lambda" {
 resource "aws_lambda_function" "new_lambda" {
   count = length(try([data.aws_lambda_function.existing_lambda.arn], [])) == 0 ? 1 : 0
 
-  function_name = var.lambda_function_name
+  function_name = local.lambda_function_name
   role          = aws_iam_role.lambda_exec.arn
   package_type  = "Image"
   image_uri     = var.instance_type == "google_drive" ? "aws_account_id.dkr.ecr.us-east-1.amazonaws.com/lambda-docker-google-drive:latest" : "aws_account_id.dkr.ecr.us-east-1.amazonaws.com/lambda-docker-s3-bucket:latest"
