97cfe5269eee29938ad006f69270df130eda0a13
ahmedX999 <aaittaleb.354@gmail.com>
2024-06-10 14:13:59 +0100

    first commit

--- main.tf
+++ main.tf
@@ -29,28 +29,14 @@ variable "instance_type" {
   type        = string
 }
 
-# Generate a random string for unique naming
-resource "random_string" "lambda_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
 locals {
   lambda_function_name = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda"
   iam_role_name        = "${var.user_id}_${var.instance_id}_${var.instance_type}_lambda_role"
 }
 
-# Check if IAM role already exists
-data "aws_iam_role" "existing_lambda_exec_role" {
-  name = local.iam_role_name
-  # If the role does not exist, ignore errors
-  depends_on = [aws_iam_role.lambda_exec_role]
-}
-
+# IAM Role for Lambda execution
 resource "aws_iam_role" "lambda_exec_role" {
-  count = data.aws_iam_role.existing_lambda_exec_role.arn != "" ? 0 : 1
-  name  = local.iam_role_name
+  name = local.iam_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -68,23 +54,14 @@ resource "aws_iam_role" "lambda_exec_role" {
 
 # Attach policy to IAM role
 resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
-  count      = data.aws_iam_role.existing_lambda_exec_role.arn != "" ? 0 : 1
-  role       = local.iam_role_name
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Check if Lambda function already exists
-data "aws_lambda_function" "existing_lambda_function" {
-  function_name = local.lambda_function_name
-  # If the function does not exist, ignore errors
-  depends_on = [aws_lambda_function.my_lambda]
 }
 
 # Lambda function resource
 resource "aws_lambda_function" "my_lambda" {
-  count         = data.aws_lambda_function.existing_lambda_function.arn != "" ? 0 : 1
   function_name = local.lambda_function_name
-  role          = data.aws_iam_role.existing_lambda_exec_role.arn != "" ? data.aws_iam_role.existing_lambda_exec_role.arn : aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_exec_role.arn
   package_type  = "Image"
   image_uri     = var.instance_type == "google_drive" ? var.ecr_repository_uri_google_drive : var.ecr_repository_uri_s3_bucket
 }
